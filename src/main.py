6f7c777c6160cb65d16b235ed8e09fb7a9585a90
andrinr <andrinrehmann@gmail.com>
2023-01-11 14:42:46 +0100

    .gitignore

--- src/main.py
+++ src/main.py
@@ -10,10 +10,10 @@ from utils.utils import even_uniform, even_exponential
 from denoising import DenoisingModel
 
 @hydra.main(version_base=None, config_path="conf", config_name="config")
-def my_app(cfg : DictConfig) -> None:
+def DDSNG(cfg : DictConfig) -> None:
     print(OmegaConf.to_yaml(cfg))
 
     #denoisingModel = DenoisingModel(cfg)
 
 if __name__ == "__main__":
-    my_app()
+    DDSNG()
