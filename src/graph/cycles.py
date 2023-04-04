5018311513ac9e05657be5ca193db24fdb35e7fa
andrinr <andrinrehmann@gmail.com>
2023-04-04 17:20:40 +0200

    rocha thatte

--- src/graph/cycles.py
+++ src/graph/cycles.py
@@ -1,3 +1,10 @@
-import networkx as nx
 
+import torch
+from torch_geometric.data import Data
+from torch_geometric.transforms import BaseTransform
 
+class SpringTransform(BaseTransform):
+    # implement this
+    # https://en.wikipedia.org/wiki/Rocha%E2%80%93Thatte_cycle_detection_algorithm
+
+    
