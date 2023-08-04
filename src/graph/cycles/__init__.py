d9b0f269b5fe8ae91378a33cc2767302c5360712
andrinr <andrinrehmann@gmail.com>
2023-08-04 12:45:30 +0200

    enrich graph with simple cycles

--- src/graph/cycles/__init__.py
+++
@@ -1,1 +0,0 @@
-from .rocha_thatte import *
--- src/graph/helpers.py
+++ src/graph/helpers.py
@@ -1,5 +1,7 @@
 from torch_geometric.data import Data
 import numpy as np
+from torch_geometric.utils import to_networkx, remove_self_loops
+from networkx.algorithms.cycles import simple_cycles
 
 def check_edge_exists(data : Data, u : int, v : int):
     edge_index = data.edge_index
@@ -20,7 +22,7 @@ def get_neighbors(data : Data, u : int):
     node_edges = src == u
     return dst[node_edges]
 
-def get_edge_index(data, u, v):
+def get_edge_index(data : Data, u, v):
     edge_index = data.edge_index
     src, dst = edge_index
     node_edges = src == u
@@ -28,3 +30,21 @@ def get_edge_index(data, u, v):
     index = np.where(node_edges)[0]
 
     return index
+
+def get_cycles(data : Data, degree : int):
+    G = to_networkx(data)
+    cycles = simple_cycles(G, length_bound=degree)
+    
+    # get signs of cycles
+    cycles = list(cycles)
+    cycle_signs = []
+    node_attr = np.
+
+    for cycle in cycles:
+        sign = 1
+        for i in range(len(cycle)):
+            u = cycle[i]
+            v = cycle[(i+1) % len(cycle)]
+            index = get_edge_index(data, u, v)
+            sign *= data.edge_attr[index]
+        
