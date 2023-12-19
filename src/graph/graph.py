fix gradient bug with degree multiplier by precomputing values

src/graph/graph.py
@@ -16,7 +16,7 @@ class Measures(NamedTuple):
 def init_measures(values : jnp.ndarray):
     max = jnp.max(values)
     avg = jnp.mean(values)
-    percentile = jnp.percentile(values, 90)
+    percentile = jnp.percentile(values, 80)
 
     return Measures(max, avg, percentile, values)
 
@@ -72,9 +72,11 @@ def to_SignedGraph(
         centrality = centrality.at[edge_index[0]].add(edge_centrality)
         centrality = centrality / degree_measures.max
 
-    centrality_measures = init_measures(jnp.expand_dims(centrality, axis=1))
+    centrality = jnp.expand_dims(centrality, axis=1)
+    centrality_measures = init_measures(centrality)
 
-    print(centrality_measures)
+    centrality = jnp.minimum(centrality, centrality_measures.percentile) / centrality_measures.percentile
+    centrality_measures = centrality_measures._replace(values=centrality)
 
     # #plot deg and centralities
     # # two subplots

src/simulation/springs.py
@@ -23,17 +23,14 @@ def update_spring_state(
 
     node_accelerations = jnp.zeros_like(spring_state.position)
     node_accelerations = node_accelerations.at[graph.edge_index[0]].add(edge_acceleration)
-    factor = (force_params.degree_multiplier * \
-              min(graph.centrality.values, graph.centrality.percentile) \
-                / graph.centrality.percentile + 1)
-    node_accelerations = node_accelerations * factor
+    node_accelerations = node_accelerations * (graph.centrality.values * force_params.degree_multiplier + 1)
 
     velocity = spring_state.velocity * (1 - simulation_params.damping)
     velocity = velocity + simulation_params.dt * node_accelerations
 
-    velocity_magnitude = jnp.linalg.norm(velocity, axis=1, keepdims=True)
-    # limit the velocity to a maximum value
-    velocity = velocity * min(velocity_magnitude, 20) / (velocity_magnitude + 1.0)
+    # velocity_magnitude = jnp.linalg.norm(velocity, axis=1, keepdims=True)
+    # # limit the velocity to a maximum value
+    # velocity = velocity * jnp.minimum(velocity_magnitude, 20) / (velocity_magnitude + 1.0)
     
     position = spring_state.position + simulation_params.dt * velocity
 