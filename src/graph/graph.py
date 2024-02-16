fix data leakage

src/graph/graph.py
@@ -29,6 +29,7 @@ class SignedGraph(NamedTuple):
     sign_one_hot : jnp.ndarray
     degree : Measures
     neg_degree : Measures
+    pos_degree : Measures
     centrality : Measures
     num_nodes : int
     num_edges : int
@@ -59,12 +60,37 @@ def to_SignedGraph(
     test_mask = jnp.array(test_mask)
     train_mask = jnp.array(train_mask)
 
+    signs_train = jnp.where(train_mask, signs, 0)
+
     node_degrees = jnp.bincount(edge_index[0])
 
+    centrality = node_degrees
+    centrality = centrality / jnp.max(centrality)
+    for i in range(3):
+        edge_centrality = centrality[edge_index[1]]
+        centrality = centrality.at[edge_index[0]].add(edge_centrality)
+        centrality = centrality / jnp.max(centrality)
+
+    #centrality = jnp.expand_dims(centrality, axis=1)
+    centrality_measures = init_measures(jnp.expand_dims(centrality, axis=1))
+
+    centrality = jnp.minimum(centrality, centrality_measures.percentile) / centrality_measures.percentile
+    centrality_measures = centrality_measures._replace(values=jnp.expand_dims(centrality, axis=1))
+
+
+    print(jnp.sum(node_degrees == 0))
     node_neg_degrees = jnp.zeros(node_degrees.shape)
-    node_neg_degrees = node_neg_degrees.at[edge_index[0]].add(signs < 0)
+    node_neg_degrees = node_neg_degrees.at[edge_index[0]].add(signs_train < 0)
     node_neg_degrees = node_neg_degrees / node_degrees
     neg_degree_measures = init_measures(jnp.expand_dims(node_neg_degrees, axis=1))
+
+    node_pos_degrees = jnp.zeros(node_degrees.shape)
+    node_pos_degrees = node_pos_degrees.at[edge_index[0]].add(signs_train > 0)
+    node_pos_degrees = node_pos_degrees / node_degrees
+    pos_degree_measures = init_measures(jnp.expand_dims(node_pos_degrees, axis=1))
+
+    print(neg_degree_measures)
+    print(pos_degree_measures)
 
 
     num_nodes = jnp.max(edge_index) + 1
@@ -77,18 +103,8 @@ def to_SignedGraph(
     degree_measures = degree_measures._replace(values=node_degrees)
 
 
-    centrality = node_degrees
-    centrality = centrality / degree_measures.max
-    for i in range(3):
-        edge_centrality = centrality[edge_index[1]]
-        centrality = centrality.at[edge_index[0]].add(edge_centrality)
-        centrality = centrality / degree_measures.max
-
-    centrality = jnp.expand_dims(centrality, axis=1)
-    centrality_measures = init_measures(centrality)
-
-    centrality = jnp.minimum(centrality, centrality_measures.percentile) / centrality_measures.percentile
-    centrality_measures = centrality_measures._replace(values=centrality)
+   
+    print(centrality_measures.values)
 
     # #plot deg and centralities
     # # two subplots
@@ -106,6 +122,7 @@ def to_SignedGraph(
         signs_one_hot,
         degree_measures,
         neg_degree_measures,
+        pos_degree_measures,
         centrality_measures,
         num_nodes, 
         num_edges,

src/main.py
@@ -29,11 +29,11 @@ def main(argv) -> None:
     EMBEDDING_DIM = 20
     INIT_POS_RANGE = 1.0
     TEST_DT = 0.001
-    DAMPING = 0.1
+    DAMPING = 0.02
     CENTERING = 0.15
 
     # Training parameters
-    NUM_EPOCHS = 40
+    NUM_EPOCHS = 50
     MULTISTPES_GRADIENT = 1
     GRAPH_PARTITIONING = False
     BATCH_NUMBER = 15
@@ -51,7 +51,7 @@ def main(argv) -> None:
     os.environ["XLA_PYTHON_CLIENT_MEM_FRACTION"]=".95"
     #os.environ["XLA_PYTHON_CLIENT_ALLOCATOR"]="platform"
 
-    #jax.config.update("jax_enable_x64", True)
+    jax.config.update("jax_enable_x64", True)
     
     dataset, dataset_name = get_dataset(DATA_PATH, argv) 
     if not is_undirected(dataset.edge_index) and TREAT_AS_UNDIRECTED:
@@ -98,31 +98,23 @@ def main(argv) -> None:
     else:
         heuristic_force_params = sm.HeuristicForceParams(
             friend=sm.MLP(
-                w0=jax.random.normal(random.PRNGKey(0), (5, 5)),
-                b0=jnp.zeros(5),
-                w1=jax.random.normal(random.PRNGKey(1), (5,1)),
+                w0=jax.random.normal(random.PRNGKey(0), (7, 4)),
+                b0=jnp.zeros(4),
+                w1=jax.random.normal(random.PRNGKey(1), (4,1)),
                 b1=jnp.zeros(1)),
             neutral=sm.MLP(
-                w0=jax.random.normal(random.PRNGKey(2), (5, 5)),
-                b0=jnp.zeros(5),
-                w1=jax.random.normal(random.PRNGKey(3), (5,1)),
+                w0=jax.random.normal(random.PRNGKey(2), (7, 4)),
+                b0=jnp.zeros(4),
+                w1=jax.random.normal(random.PRNGKey(3), (4,1)),
                 b1=jnp.zeros(1)),
             enemy=sm.MLP(
-                w0=jax.random.normal(random.PRNGKey(4), (5, 5)),
-                b0=jnp.zeros(5),
-                w1=jax.random.normal(random.PRNGKey(5), (5,1)),
+                w0=jax.random.normal(random.PRNGKey(4), (7, 4)),
+                b0=jnp.zeros(4),
+                w1=jax.random.normal(random.PRNGKey(5), (4,1)),
                 b1=jnp.zeros(1)),
-                
         )
 
         print("no spring params checkpoint found, using default params")
-
-
-    simulation_params_train = sm.SimulationParams(
-        iterations=PER_EPOCH_SIM_ITERATIONS // MULTISTPES_GRADIENT,
-        dt=TRAIN_DT,
-        damping=DAMPING,
-        centering=CENTERING)
 
     # Create initial values for neural network parameters
     key_force, key_training, key_test = random.split(random.PRNGKey(2), 3)
@@ -130,14 +122,20 @@ def main(argv) -> None:
     force_params = heuristic_force_params
 
     if OPTIMIZE_FORCE_PARAMS:
-        
+        simulation_params_train = sm.SimulationParams(
+            iterations=PER_EPOCH_SIM_ITERATIONS,
+            dt=TRAIN_DT,
+            damping=DAMPING,
+            centering=CENTERING)
+
+    
         force_params, loss_hist, metrics_hist, force_params_hist = sm.train(
             random_key=key_training,
             batches=batches,
             force_params=force_params,
             training_params= sm.TrainingParams(
                 num_epochs=NUM_EPOCHS,
-                learning_rate=0.02,
+                learning_rate=0.04,
                 batch_size=BATCH_NUMBER,
                 init_pos_range=INIT_POS_RANGE,
                 embedding_dim=EMBEDDING_DIM,

src/simulation/springs.py
@@ -52,12 +52,23 @@ def acceleration(
     degree_j = graph.degree.values[graph.edge_index[1]]
     degree_i_neg = graph.neg_degree.values[graph.edge_index[0]]
     degree_j_neg = graph.neg_degree.values[graph.edge_index[1]]
+    degree_i_pos = graph.pos_degree.values[graph.edge_index[0]]
+    degree_j_pos = graph.pos_degree.values[graph.edge_index[1]]
 
     spring_vector = position_j - position_i
     distance = jnp.linalg.norm(spring_vector, axis=1, keepdims=True)
     spring_vector_norm = spring_vector / (distance + EPSILON)
 
-    force_factor = heuristic_force(params, degree_i, degree_j, degree_i_neg, degree_j_neg, distance, graph.sign)
+    force_factor = heuristic_force(
+        params, 
+        degree_i, 
+        degree_j, 
+        degree_i_neg, 
+        degree_j_neg, 
+        degree_i_pos,
+        degree_j_pos,
+        distance, 
+        graph.sign)
 
     return force_factor * spring_vector_norm
 
@@ -67,24 +78,29 @@ def heuristic_force(
     degree_j : jnp.ndarray,
     degree_negative_i : jnp.ndarray,
     degree_negative_j : jnp.ndarray,
+    degree_positive_i : jnp.ndarray,
+    degree_positive_j : jnp.ndarray,
     distance : jnp.ndarray,
     sign : jnp.ndarray
 ) -> jnp.ndarray:
     
-    input = jnp.concatenate([degree_i, degree_j, degree_negative_i, degree_negative_j, distance], axis=1)
+    input = jnp.concatenate([
+        degree_i, degree_j, 
+        degree_negative_i, degree_negative_j, 
+        degree_positive_i, degree_positive_j,
+        distance], axis=1)
 
     friend = jnp.dot(input, params.friend.w0) + params.friend.b0
-    friend = jax.nn.relu(friend)
+    # friend = jax.nn.relu(friend)
     friend = jnp.dot(friend, params.friend.w1) + params.friend.b1
 
     neutral = jnp.dot(input, params.neutral.w0) + params.neutral.b0
-    neutral = jax.nn.relu(neutral)
+    # neutral = jax.nn.relu(neutral)
     neutral = jnp.dot(neutral, params.neutral.w1) + params.neutral.b1
   
     enemy = jnp.dot(input, params.enemy.w0) + params.enemy.b0
-    enemy = jax.nn.relu(enemy)
+    # enemy = jax.nn.relu(enemy)
     enemy = jnp.dot(enemy, params.enemy.w1) + params.enemy.b1
-   
 
     # friend = jnp.maximum(distance - params.friend.rest_length, 0) * \
     #     params.friend.attraction_stiffness * (1 + params.friend.degree_i_multiplier * degree_i) * (1 + params.friend.degree_j_multiplier * degree_j)

src/simulation/train.py
@@ -3,6 +3,7 @@ import jax
 import jax.numpy as jnp
 from tqdm import tqdm
 import optax
+from jax import custom_vjp
 
 import graph as g
 import simulation as sm
@@ -15,13 +16,26 @@ class TrainingParams(NamedTuple):
     embedding_dim : int
     multi_step : int
 
+@custom_vjp
+def clip_gradient(lo, hi, x):
+  return x  # identity function
+
+def clip_gradient_fwd(lo, hi, x):
+  return x, (lo, hi)  # save bounds as residuals
+
+def clip_gradient_bwd(res, g):
+  lo, hi = res
+  return (None, None, jnp.clip(g, lo, hi))  # use None to indicate zero cotangents for lo and hi
+
+clip_gradient.defvjp(clip_gradient_fwd, clip_gradient_bwd)
+
 def train(
     random_key : jax.random.PRNGKey,
     batches : list[g.SignedGraph],
     force_params : sm.HeuristicForceParams,
     training_params : TrainingParams,
     simulation_params : sm.SimulationParams,
-) -> (sm.HeuristicForceParams, list[float], list[sm.Metrics]):
+) -> tuple[sm.HeuristicForceParams, list[float], list[sm.Metrics]]:
 
     print("\n Training...")
 
@@ -63,6 +77,10 @@ def train(
                 spring_state, #1
                 force_params, #2
                 batch_graph)
+            
+            grad = clip_gradient(-1, 1, grad)
+            
+
         
             # print(force_params)
             # print(grad)