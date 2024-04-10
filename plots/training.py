321c6b0407884e596e0fb4a8843ab7c301ad02a0
andrinr <andrinrehmann@gmail.com>
2024-04-10 16:49:06 +0200

    plot adjustements

--- plots/training.py
+++ plots/training.py
@@ -15,28 +15,25 @@ plt.show()
 #colors = plt.cm.get_cmap('Set1', 10)
 colors = ['#d73027', '#4575b4', '#91bfdb', '#984ea3', '#ff7f00']
 # Three subplots with shared x axis
-fig, (ax1, ax2, ax3) = plt.subplots(3, 1, sharex=True)
+fig, (ax1, ax2, ax3, ax4) = plt.subplots(4, 1, sharex=True)
 # plot metrics over time
 ax1.plot(epochs, data.auc, color=colors[0])
 ax1.plot(epochs, data.f1_macro, color=colors[1])
+# shared x axis
 # set legend position top right
 
 ax1.legend(['AUC', 'F1 macro'], loc='lower right')
-ax1.set_xlabel('epochs')
-ax1.set_ylabel('accuracy')
 
-ax2.plot(epochs, data.dt, color=colors[0])
-ax2.plot(epochs, data.damping, color=colors[1])
-ax2.set_xlabel('epochs')
-ax2.set_ylabel('values')
+ax2.plot(epochs, data.loss, color=colors[0])
+ax2.legend(['loss'], loc='upper right')
 
-ax2.legend(['dt', 'damping'], loc='upper right')
-ax2.set_xlabel('epochs')
-ax2.set_ylabel('values')
+ax3.plot(epochs, data.dt, color=colors[0])
+ax3.plot(epochs, data.damping, color=colors[1])
 
-ax3.plot(epochs, data.iterations, color=colors[0])
-ax3.legend(['iterations'], loc='lower right')
-ax3.set_xlabel('epochs')
-ax3.set_ylabel('values')
+ax3.legend(['dt', 'damping'], loc='upper right')
+
+ax4.plot(epochs, data.iterations, color=colors[0])
+ax4.legend(['iterations'], loc='lower right')
+ax4.set_xlabel('epochs')
 
 plt.show()
